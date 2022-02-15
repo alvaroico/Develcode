23f8300362a1ad169f44da8eadf3e7ad22ac1df1
Alvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2022-02-15 16:05:50 -0300

    Inserindo Usuarios

--- backend/src/Controllers/User.ts
+++ backend/src/Controllers/User.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from "express";
+import { PoolPostgreSQL, QueryResult } from "../connection/PostgreSQL";
 import { User } from "../interfaces/User";
 // import { MascaraTelefoneNonoDigito } from "../tools/Ferramentas";
 
@@ -10,9 +11,31 @@ const Cadastro = async (
   const { usuarios } = request.body as { usuarios: Array<User> };
 
   if (usuarios != undefined && usuarios.length > 0) {
-    response.status(200).json({
-      message: "Usuario Cadastrados",
-    });
+    for await (const usuario of usuarios) {
+      await PoolPostgreSQL(`INSERT INTO public.usuario
+      (nome, nascimento, foto)
+      VALUES('${usuario.nome}', '${usuario.nascimento}', '${usuario.foto}')`)
+        .then((result) => {
+          const PoolPostgreSQLRetorno = result as QueryResult;
+          if (
+            PoolPostgreSQLRetorno.rowCount == 0 &&
+            PoolPostgreSQLRetorno.rowCount == undefined
+          ) {
+            response.status(400).json({
+              message: `Erro ao Salvar no PoolPostgreSQL ${PoolPostgreSQLRetorno}`,
+            });
+          } else {
+            response.status(200).json({
+              message: "Usuario Cadastrados",
+            });
+          }
+        })
+        .catch((error) => {
+          response.status(400).json({
+            message: `Erro ao Salvar no PoolPostgreSQL ${error}`,
+          });
+        });
+    }
   } else {
     response.status(400).json({
       message: "Usuario não informados",
